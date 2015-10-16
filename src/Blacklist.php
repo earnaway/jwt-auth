07d7ee37509336ab9e9093259a6550f135cdea4f
Mike Salmon <m.salmon@earnaway.com>
2015-10-16 08:51:21 +0100

    Added a grace perioid before the token is blacklisted

--- src/Blacklist.php
+++ src/Blacklist.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of jwt-auth
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Tymon\JWTAuth;
 
 use Tymon\JWTAuth\Providers\Storage\StorageInterface;
@@ -7,12 +14,27 @@ use Tymon\JWTAuth\Providers\Storage\StorageInterface;
 class Blacklist
 {
     /**
-     * @var \Tymon\JWTAuth\Providers\Storage\StorageInterface
+     * @var StorageInterface
      */
     protected $storage;
 
     /**
-     * @param \Tymon\JWTAuth\Providers\Storage\StorageInterface  $storage
+     * The grace period when a token is blacklisted. In seconds
+     *
+     * @var integer
+     */
+    protected $gracePeriod = 0;
+
+    /**
+     * Number of minutes from issue date in which a JWT can be refreshed.
+     *
+     * @var integer
+     */
+    protected $refreshTTL = 20160;
+
+    /**
+     * @param StorageInterface  $storage
+     *
      */
     public function __construct(StorageInterface $storage)
     {
@@ -23,22 +45,24 @@ class Blacklist
      * Add the token (jti claim) to the blacklist
      *
      * @param  \Tymon\JWTAuth\Payload  $payload
+     *
      * @return boolean
      */
     public function add(Payload $payload)
     {
         $exp = Utils::timestamp($payload['exp']);
+        $refreshExp = Utils::timestamp($payload['iat'])->addMinutes($this->refreshTTL);
 
-        // there is no need to add the token to the blacklist
-        // if the token has already expired
-        if ($exp->isPast()) {
-            return false;
-        }
+        // get the later of the two expiration dates
+        $lastExp = $exp->max($refreshExp);
 
-        // add a minute to abate potential overlap
-        $minutes = $exp->diffInMinutes(Utils::now()->subMinute());
-
-        $this->storage->add($payload['jti'], [], $minutes);
+        // find the number of minutes until the expiration date, plus 1 minute to avoid overlap
+        $minutes = $lastExp->diffInMinutes(Utils::now()->subMinute());
+        $this->storage->add(
+            $payload['jti'],
+            ['valid_until' => $this->getGraceTimestamp()],
+            $minutes
+        );
 
         return true;
     }
@@ -51,7 +75,12 @@ class Blacklist
      */
     public function has(Payload $payload)
     {
-        return $this->storage->has($payload['jti']);
+        $grace = $this->storage->get($payload['jti']);
+        // check whether the expiry + grace has past
+        if (is_null($grace) || Utils::timestamp($grace['valid_until'])->isFuture()) {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -73,7 +102,41 @@ class Blacklist
     public function clear()
     {
         $this->storage->flush();
-
         return true;
+    }
+
+    /**
+     * Get the timestamp when the blacklist comes into effect
+     * This defaults to immediate (0 seconds)
+     *
+     * @return integer
+     */
+    protected function getGraceTimestamp()
+    {
+        return (int) Utils::now()->addSeconds($this->gracePeriod)->format('U');
+    }
+
+    /**
+     * Set the grace period
+     *
+     * @param  integer
+     * @return Blacklist
+     */
+    public function setGracePeriod($gracePeriod)
+    {
+        $this->gracePeriod = (int) $gracePeriod;
+        return $this;
+    }
+
+    /**
+     * Set the refresh time limit
+     *
+     * @param  integer
+     * @return Blacklist
+     */
+    public function setRefreshTTL($ttl)
+    {
+        $this->refreshTTL = (int) $ttl;
+        return $this;
     }
 }
